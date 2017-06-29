415203d7b5135f67dc14ea930579143eeb9fa228
Amanda Vieira <amandasv.pm@gmail.com>
2017-06-29 13:56:28 -0300

    create migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629164900) do
+ActiveRecord::Schema.define(version: 20170629165402) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
